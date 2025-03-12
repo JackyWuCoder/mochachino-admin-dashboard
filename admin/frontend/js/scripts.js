e8c75c1aa75f3bdf157cd4fd9d261a59c1063477
Jacky Wu <JackyWuCoder@gmail.com>
2025-03-12 02:10:31 -0700

    feat: map button actions to api endpoints

--- admin/frontend/js/scripts.js
+++ admin/frontend/js/scripts.js
@@ -20,8 +20,27 @@ function closeSidebar() {
 document.querySelectorAll(".action-btn").forEach(button => {
   button.addEventListener("click", async (e) => {
       const action = e.target.innerText.trim();
+
       let apiURL = "";
       let details = prompt(`Enter details for ${action}`);
+
       if (!details) return alert("Details are required!");
+
+      switch (action) {
+        case 'Add Artwork':
+            apiUrl = '/api/artwork';
+            break;
+        case 'Update Commission Status':
+            apiUrl = '/api/commission';
+            break;
+        case 'Add Convention':
+            apiUrl = '/api/convention';
+            break;
+        case 'Write Blog Post':
+            apiUrl = '/api/blog';
+            break;
+        default:
+            return;
+    }
   })
 })
