f89ace480bc436b79d5fac3830181faf54e77e5f
Jacky Wu <JackyWuCoder@gmail.com>
2025-03-11 23:43:45 -0700

    fix: changed the active class name from "sidebar-active" to "sidebar-responsive"

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -5,14 +5,14 @@ const sidebar = document.getElementById("sidebar");
 
 function openSidebar() {
   if (!sidebarOpen) {
-    sidebar.classList.add("sidebar-active");
+    sidebar.classList.add("sidebar-responsive");
     sidebarOpen = true;
   }
 }
 
 function closeSidebar() {
   if (sidebarOpen) {
-    sidebar.classList.remove("sidebar-active");
+    sidebar.classList.remove("sidebar-responsive");
     sidebarOpen = false;
   }
 }
