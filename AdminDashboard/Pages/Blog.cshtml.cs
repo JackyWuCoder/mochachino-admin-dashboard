0f1db0019cf40bd7e1269ed833f6e46446f3a7e5
Jacky Wu <JackyWuCoder@gmail.com>
2025-03-18 00:06:40 -0700

    feat: created basic template Razor Pages for Comissions, Conventions, and Blogs

---
+++ AdminDashboard/Pages/Blog.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class BlogModel : PageModel
+{
+    public void OnGet()
+    {
+        // Placeholder for future logic
+    }
+}
---
+++ AdminDashboard/Pages/Conventions.cshtml.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class ConventionsModel : PageModel
+{
+    public void OnGet()
+    {
+        // Placeholder for future logic
+    }
+}
